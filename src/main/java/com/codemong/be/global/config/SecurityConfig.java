6f340fe7d69a146230693b515f72cc7a433a0b55
최현철 <guscjf1211@naver.com>
2026-06-19 15:09:50 +0900

    commit

--- src/main/java/com/codemong/be/global/config/SecurityConfig.java
+++ src/main/java/com/codemong/be/global/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173"
-        ))
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
