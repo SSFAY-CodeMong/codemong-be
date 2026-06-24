aeb264c88e7752c8eef5de2b75cfcf1ed4dec723
최현철 <guscjf1211@naver.com>
2026-06-24 15:03:40 +0900

    remove setup dir

--- src/main/java/com/codemong/be/setup/repository/SetupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codemong.be.setup.repository;
-
-import com.codemong.be.setup.entity.Setup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SetupRepository extends JpaRepository<Setup, Long> {
-}
